7bc6c9944ffb585c4d4deb38962485bfdeb4ac12
Patuljak <patuljakiztuk@gmail.com>
2023-01-06 08:48:02 +0100

    Add maven-publish plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.8.0-RC"
+    `maven-publish`
 }
 
 group = "com.github.malipatuljak"
@@ -17,6 +18,18 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "com.github.MaliPatuljak"
+            artifactId = "MinifyJava"
+            version = "1.0.0"
+
+            from(components["java"])
+        }
+    }
 }
 
 tasks.withType<KotlinCompile> {
